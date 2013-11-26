f818c309ac779da0306fae832c921bd76fc05613
Mike Wilcox <mwilcox56@gmail.com>
2013-11-25 19:57:30 -0500

    add chatService service

---
+++ app/scripts/services/chatService.js
@@ -0,0 +1,34 @@
+app.service('chatService', function ($rootScope, $location) {
+
+    //var url = location.origin.replace(/^http/, 'ws');
+    var url = ['ws://', $location.host(), ':', $location.port()].join('');
+    console.log('Url: ', url)
+    var ws = new WebSocket(url);
+
+    // messages from node server
+    ws.onmessage = function (event) {
+        var server_response = JSON.parse(event.data);
+        if (server_response != null && server_response.data != null) {
+            switch (server_response.data) {
+                case 0: // user information
+                    $rootScope.$broadcast('userJoined', server_response);
+                    break;
+                case 1: // song information
+                    $rootScope.$broadcast('songRequested', server_response);
+                    break;
+                case 2: // chat message
+                    $rootScope.$broadcast('chatMessageReceived', server_response);
+                    break;
+            }
+        }
+    };
+
+    function send(message) {
+        $rootScope.$broadcast('sending', message);
+        ws.send(message);
+    }
+
+    return {
+        send: send
+    };
+})
