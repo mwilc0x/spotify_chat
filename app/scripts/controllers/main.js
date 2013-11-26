260ff717b0bb7749e19cc1530e23c198188676ad
Mike Wilcox <mwilcox56@gmail.com>
2013-11-25 19:58:22 -0500

    separate out service code and modify ws url

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,85 +1,81 @@
 'use strict';
 
-  app.controller('MainCtrl', function MainCtrl($scope, $modal) {
-        $scope.host = location.origin.replace(/^http/, 'ws');
-        $scope.ws = new WebSocket($scope.host);
+app.controller('MainCtrl', function MainCtrl($log, $scope, $modal, messageType, chatService, audio) {
+    $scope.user = {};
+    $scope.current_users = [{ data: 0, name: "Bot", id: 0 }];
+    $scope.text = "";
+    $scope.messages = [{
+        name: "Bot",
+        text: 'Welcome! To play a song, type !spotify song or artist'
+    }];
+    $scope.chat_history = [{ text: "" }];
+    $scope.chat_history_index = -1;
 
-        $scope.user = {};
-        $scope.current_users = [{data: 0, name: "Bot", id: 0}];
-        $scope.text = "";
-        $scope.messages =  [{
-            name: "Bot",
-            text: "Welcome! Play music by entering artist or song name"
-        }];
-        $scope.chat_history = [{text: ""}];
-        $scope.chat_history_index = -1;
+    $scope.chatInputFocused = false;
 
+    $scope.loseFocus = function () {
         $scope.chatInputFocused = false;
+    };
 
-        $scope.loseFocus = function() {
-            $scope.chatInputFocused = false;
-        };
+    // opening || closing of modal window
+    $scope.open = function () {
+        var modalInstance = $modal.open({
+            templateUrl: 'views/register.html',
+            controller: 'modalController'
+        });
 
-        // opening || closing of modal window
-        $scope.open = function () {
-            var modalInstance = $modal.open({
-                templateUrl: 'views/register.html',
-                controller: 'modalController'
-            });
-
-
-            modalInstance.result.then(function (nickname) {
-                for (var i = 0; i < $scope.current_users.length; i++) {
-                    if($scope.current_users[i].id == $scope.user.id) {
-                        $scope.current_users[i].name = nickname;
-                    }
-                }
-
-                $scope.chatInputFocused = true;
-
-                $scope.user.name = nickname;
-            }, function () {
-                console.log('Modal dismissed at: ' + new Date());
-            });
-        };
-
-        // messages from node server
-        $scope.ws.onmessage = function (event) {
-            var server_response = JSON.parse(event.data);
-
-            if(server_response != null && server_response.data != null) {
-                switch(server_response.data) {
-                    case 0: // user information
-                        $scope.user = server_response;
-                        $scope.current_users.push(server_response);
-                        break;
-                    case 1: // song information
-                        if($scope.audio != null) {
-                            $scope.audio.pause();
-                        }
-
-                        $scope.audio = new Audio;
-                        $scope.audio.src = "/audio/" + server_response.song;
-                        $scope.audio.autoplay = true;
-                        $scope.audio.controls = true;
-                        console.log("audio started");
-                        break;
+        modalInstance.result.then(function (nickname) {
+            for (var i = 0; i < $scope.current_users.length; i++) {
+                if ($scope.current_users[i].id == $scope.user.id) {
+                    $scope.current_users[i].name = nickname;
                 }
             }
-        };
 
-        $scope.submit = function() {
-             if (this.text) {
-                 $scope.ws.send(this.text);
-                 $scope.chat_history.unshift({text: this.text});
-                 $scope.messages.push({name: $scope.user.name, text: this.text});
-                 $scope.text = "";
-             }
-        };
+            $scope.chatInputFocused = true;
 
-        // page load
-        var init = function () {
-            $scope.open();
-        };
-        init();
-  });
+            $scope.user.name = nickname;
+        }, function () {
+            $log.log('Modal dismissed at: ' + new Date());
+        });
+    };
+
+    function onUserJoined(user) {
+        $scope.user = user;
+        $scope.current_users.push(user);
+        $log.log("new user joined");
+        $scope.$apply();
+    }
+
+    function onSongRequested(songRequest) {
+        $log.log("in audio case song: " + songRequest.song + " user: " + songRequest.user);
+        audio.play("/audio/" + songRequest.song);
+        $scope.messages.push({ name: songRequest.user, text: songRequest.song });
+        $log.log("audio started");
+        $scope.$apply();
+    }
+
+    function onChatMessageReceived(message) {
+        $scope.messages.push({ name: message.user, text: message.text });
+        $scope.$apply();
+    }
+
+    $scope.$on('userJoined', function (evt, user) { onUserJoined(user); });
+    $scope.$on('songRequested', function (evt, songRequest) { onSongRequested(songRequest); });
+    $scope.$on('chatMessageReceived', function (evt, message) { onChatMessageReceived(message); });
+
+    $scope.submit = function () {
+        if (this.text) {
+            var msgType = messageType.decipher(this.text);
+            var message = JSON.stringify({ data: msgType.data, text: msgType.text, user: $scope.user.name });
+            chatService.send(message);
+            $scope.chat_history.unshift({ text: this.text });
+            $scope.text = "";
+        }
+    };
+
+    // page load
+    var init = function () {
+        $scope.open();
+    };
+    init();
+});
