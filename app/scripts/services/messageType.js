e73ffe8c8d6f26421c602fb7e729b2760f3f7013
Mike Wilcox <mwilcox56@gmail.com>
2013-11-27 17:59:16 -0500

    change how to call spotify, bot play is easier to type than !spotify

--- app/scripts/services/messageType.js
+++ app/scripts/services/messageType.js
@@ -2,7 +2,7 @@ app.factory('messageType', function() {
     return {
         decipher: function(message) {
             var data;
-            if(message.search('!spotify ') != -1) {
+            if(message.search('bot play ') != -1) {
                 data = 1;
                 message = message.substring(9, message.length);
             }
