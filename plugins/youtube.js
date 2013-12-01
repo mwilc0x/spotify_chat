3853b266f649df2e8a77ea63af4898a012c0228c
Mike Wilcox <mwilcox56@gmail.com>
2013-12-01 09:29:22 -0500

    update using youtube feeds package to search youtube vids

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,1 +1,18 @@
 var youtube = require('youtube-feeds');
+
+exports.search = function(search, message, broadcast) {
+    youtube.feeds.videos( {q: search, 'max-results': 1 }, function(err,data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        if(data != null && data.items != null && data.items[0] != null) {
+            //console.log(data);
+            var title = data.items[0].title;
+            var video = JSON.stringify({data: "youtube-info", videoID: data.items[0].id, user: message.user, title: title});
+
+            if(broadcast != null) broadcast(video);
+        }
+    });
+}
