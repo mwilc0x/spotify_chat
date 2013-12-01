9c4ba06b818a9a469e1e910a33e6a5c66bff25ce
Mike Wilcox <mwilcox56@gmail.com>
2013-12-01 18:49:40 -0500

    add dist

---
+++ dist/scripts/services/8e3cdbd0.chatService.js
@@ -0,0 +1,52 @@
+app.service('chatService', function ($rootScope, $location) {
+
+    var url = ['ws://', $location.host(), ':', $location.port()].join('');
+    console.log('Url: ', url)
+    var ws = new WebSocket(url);
+
+    // messages from node server
+    ws.onmessage = function (event) {
+        var server_response = JSON.parse(event.data);
+        if (server_response != null && server_response.data != null) {
+            switch (server_response.data) {
+                case "user-list": // users in chat room
+                    $rootScope.$broadcast('userList', server_response);
+                    break;
+                case "delete-user": // user left chat room
+                    $rootScope.$broadcast('userLeft', server_response);
+                    break;
+                case "user-info": // user information
+                    $rootScope.$broadcast('userJoined', server_response);
+                    break;
+                case "song-info": // song information
+                    $rootScope.$broadcast('songRequested', server_response);
+                    break;
+                case "chat-message": // chat message
+                    $rootScope.$broadcast('chatMessageReceived', server_response);
+                    break;
+                case "user-info-change": // user info change
+                    $rootScope.$broadcast('userUpdatedInfo', server_response);
+                    break;
+                case "youtube-info": // youtube information
+                    $rootScope.$broadcast('youtubeRequested', server_response);
+                    break;
+
+            }
+        }
+    };
+
+    function send(message) {
+        $rootScope.$broadcast('sending', message);
+        ws.send(message);
+    }
+
+    function scrollDown() {
+        var objDiv = document.getElementById("chat");
+        objDiv.scrollTop = objDiv.scrollHeight;
+    }
+
+    return {
+        send: send,
+        scroll: scrollDown
+    };
+})
