acece48fbc4ac1e74ac472c20be60770b1f87f50
Mike Wilcox <mwilcox56@gmail.com>
2013-12-01 09:17:02 -0500

    add mediaPlayer directive

---
+++ app/scripts/directives/mediaPlayer.js
@@ -0,0 +1,11 @@
+app.directive('mediaPlayer', function($log) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            title: '@',
+            src: '@'
+        },
+        template: '<div class="player" ng-hide="!src"><h5>{{title}}</h5><audio autoplay controls src="{{src}}"></audio></div>'
+    };
+});
