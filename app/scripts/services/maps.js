c49ac55686688d1ad40c4f1093d2208971f5eef7
Mike Wilcox <mwilcox56@gmail.com>
2013-12-06 20:50:26 -0500

    change function name

--- app/scripts/services/maps.js
+++ app/scripts/services/maps.js
@@ -6,14 +6,14 @@ app.service('maps', function($rootScope) {
             lng: ''
         },
 
-        RestoreState: function (mapsRequest) {
+        SetCoordinates: function (mapsRequest) {
             service.model.lat = mapsRequest.lat;
             service.model.lng = mapsRequest.lng;
         }
     }
 
     $rootScope.$on('mapsRequested', function(evt, mapsRequest) {
-        service.RestoreState(mapsRequest)
+        service.SetCoordinates(mapsRequest)
     });
 
     return service;
