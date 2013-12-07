9b5faf587545b240a0f21441a1a1330a3c80fd36
Mike Wilcox <mwilcox56@gmail.com>
2013-12-06 20:24:34 -0500

    add maps service

---
+++ app/scripts/services/maps.js
@@ -0,0 +1,25 @@
+/**
+ * Created by mikewilcox on 06/12/2013.
+ */
+
+app.service('maps', function($rootScope) {
+
+    var service = {
+
+        model: {
+            lat: '',
+            lng: ''
+        },
+
+        RestoreState: function (mapsRequest) {
+            service.model.lat = mapsRequest.lat;
+            service.model.lng = mapsRequest.lng;
+        }
+    }
+
+    $rootScope.$on('mapsRequested', function(evt, mapsRequest) {
+        service.RestoreState(mapsRequest)
+    });
+
+    return service;
+});
