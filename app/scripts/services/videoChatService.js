70f0eb4b87e9d9a00637bc050bfd05891699df2b
Mike Wilcox <mwilcox56@gmail.com>
2013-12-07 18:51:23 -0500

    add video chat service

---
+++ app/scripts/services/videoChatService.js
@@ -0,0 +1,59 @@
+app.service('videoChatService', function() {
+
+    function connect() {
+        easyrtc.enableDebug(false);
+        console.log("Initializing.");
+        easyrtc.enableAudio(false);
+        easyrtc.setRoomOccupantListener(convertListToButtons);
+        easyrtc.initMediaSource(
+            function(){ 	   // success callback
+                var selfVideo = document.getElementById("selfVideo");
+                easyrtc.setVideoObjectSrc(selfVideo, easyrtc.getLocalStream());
+                easyrtc.connect("easyrtc.videoOnly", loginSuccess, loginFailure);
+            },
+            function(errorCode, errmesg){
+                easyrtc.showError("MEDIA-ERROR", errmesg);
+            }  // failure callback
+        );
+    }
+
+    function convertListToButtons (roomName, data, isPrimary) {
+        clearConnectList();
+        otherClientDiv = document.getElementById("otherClients");
+        for(var i in data) {
+            var button = document.createElement("button");
+            button.onclick = function(easyrtcid) {
+                return function() {
+                    performCall(easyrtcid);
+                };
+            }(i);
+
+            label = document.createTextNode( easyrtc.idToName(i));
+            button.appendChild(label);
+            otherClientDiv.appendChild(button);
+        }
+        if( !otherClientDiv.hasChildNodes() ) {
+            otherClientDiv.innerHTML = "<em>Nobody else is on...</em>";
+        }
+    }
+
+    function showVideoChat() {
+        var chatDiv = document.getElementById("chat");
+        var chatWindow = chatDiv.getElementsByClassName("messages");
+        var input = chatDiv.getElementsByClassName("chat-input");
+
+        chatWindow[0].style.height = "54%";
+        chatWindow[0].style.maxHeight = "56%";
+        input[0].style.position = "relative";
+    }
+
+    function hideVideoChat() {
+
+    }
+
+    return {
+        connect: connect,
+        show: showVideoChat,
+        hide: hideVideoChat
+    }
+});
