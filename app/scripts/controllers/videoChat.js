b5ca1ca494ffb2108c9bbebabb2306c7a5a9bdfc
Mike Wilcox <mwilcox56@gmail.com>
2013-12-07 19:15:45 -0500

    set video model to true when video enabled

--- app/scripts/controllers/videoChat.js
+++ app/scripts/controllers/videoChat.js
@@ -4,6 +4,8 @@ app.controller('videoChat', function videoChat($scope, videoChatService) {
     $scope.video = false;
 
     $scope.connect = function() {
+        $scope.video = true;
+        $scope.$apply();
         videoChatService.show();
         videoChatService.connect();
     };
