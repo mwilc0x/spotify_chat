ea82e1189434dcf1459c127e8aff14cba2a6142b
Mike Wilcox <mwilcox56@gmail.com>
2013-12-11 20:58:14 -0500

    changes to model to accomodate new webrtc approach

--- app/scripts/controllers/videoChat.js
+++ app/scripts/controllers/videoChat.js
@@ -1,18 +1,23 @@
 'use strict'
 app.controller('videoChat', function videoChat($scope, videoChatService) {
 
+    videoChatService.show();
+
+    $scope.template = {
+        url: "../views/videoChat.html"
+    }
+
     $scope.video = false;
 
     $scope.connect = function() {
-
-        videoChatService.show();
-        videoChatService.connect();
         $scope.video = true;
+        videoChatService.connect();
+
     };
 
     $scope.disconnect = function() {
         videoChatService.hide();
-        videoChatService.disconnect();
+        //videoChatService.disconnect();
         $scope.video = false;
     }
 });
